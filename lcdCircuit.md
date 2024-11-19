af88696bc4574b6f9072ec0789088781e4f7921c
patwilson3 <patrick.wilson2@mail.mcgill.ca>
2024-11-19 14:26:48 -0500

    Changes to be committed: 	deleted:    LCDdisplay.cpp 	deleted:    alg.py 	deleted:    button.py 	deleted:    lcdCircuit.md 	deleted:    message_tester.py 	deleted:    sendMessage.py

--- lcdCircuit.md
+++
@@ -1,1 +0,0 @@
-<img src = "https://github.com/user-attachments/assets/bf301baa-4b84-43a7-a646-b2a0c1d61877"> </img>
--- message_tester.py
+++
@@ -1,6 +0,0 @@
-import sendMessage
-import time
-
-send_message_to_ard("hello")
-time.sleep(10)
-send_message_to_ard("test")
